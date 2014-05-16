45f0b7bfae3bc49294c683621918381e1d4c2056
Saladoc <saladoc@heridan.de>
2014-05-16 18:38:07 +0200

    expand tests for full coverage of tested class

--- src/test/java/de/chaoschaot234/drillingminer/classes/ScalingKitTest.java
+++ src/test/java/de/chaoschaot234/drillingminer/classes/ScalingKitTest.java
@@ -11,6 +11,51 @@ import org.junit.Test;
 import static de.chaoschaot234.drillingminer.classes.ScalingKit.getMinimalStacks;
 
 public class ScalingKitTest {
+    
+    @Test
+    public void newKitIsEmpty() {
+        ScalingKit kit = new ScalingKit();
+        assertEquals(0, kit.materialSet().size());
+        assertEquals(0, kit.getItems(0).size());
+        assertEquals(0, kit.getItems(1000).size());
+    }
+    
+    @Test
+    public void testAddAndRemoveFlatIngredient() {
+        ScalingKit kit = new ScalingKit();
+        kit.setFlat(Material.EGG, 3);
+        assertEquals(1, kit.materialSet().size());
+        assertTrue(kit.materialSet().contains(Material.EGG));
+        kit.setFlat(Material.EGG, 0);
+        assertEquals(0, kit.materialSet().size());
+        assertFalse(kit.materialSet().contains(Material.EGG));
+    }
+    
+    @Test
+    public void testAddAndRemoveScalingIngredient() {
+        ScalingKit kit = new ScalingKit();
+        kit.setScaling(Material.EGG, 3);
+        assertEquals(1, kit.materialSet().size());
+        assertTrue(kit.materialSet().contains(Material.EGG));
+        kit.setScaling(Material.EGG, 0);
+        assertEquals(0, kit.materialSet().size());
+        assertFalse(kit.materialSet().contains(Material.EGG));
+    }
+   
+    @Test
+    public void testScaling() {
+        ScalingKit kit = new ScalingKit();
+        kit.setScaling(Material.STONE, 1);
+        for (int i = 0; i < 2500; i += 21) {
+            List<ItemStack> stacks = kit.getItems(i);
+            int count = 0;
+            for (ItemStack stack : stacks) {
+                assertEquals(Material.STONE, stack.getType());
+                count += stack.getAmount();
+            }
+            assertEquals(i, count);
+        }
+    }
 
     @Test
     public void testGetMinimalStacksAreMinimal() {
